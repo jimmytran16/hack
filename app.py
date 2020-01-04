fe84b8ce9ebce5ae268407b05b63706ee7229e45
Jimmytran16 <jimmytran1620@gmail.com>
2020-01-04 01:51:47 -0500

    second commit

--- app.py
+++ app.py
@@ -10,29 +10,48 @@ def index():
 
 @app.route('/dashboard')
 def dashboard():
-    return render_template('dashboard.html')
+    return render_template('dashboard.html',dash=True)
 
 #student behavoir
 @app.route('/behavior')
 def general():
-    return render_template('general.html')
+    return render_template('general.html',general=True)
 
 #student forms - incident / behavior
 @app.route('/forms')
 def forms():
-    return render_template('form_component.html')
+    return render_template('form_component.html',form=True)
 
 #student records - grades 
-@app.route('/students')
+@app.route('/records')
 def students():
-    return render_template('basic_table.html')
+    return render_template('basic_table.html',basic=True)
 
 @app.route('/logout')
 def logout():
     return render_template('index.html')
 
+@app.route('/login',methods=['POST'])
+def login():
+    # get the username and password from the field
+    # authenticate the username and password 
+    # if doesn't exist, return to index and send error message
+    fixed_name = 'Jimmy Tran'
+    fixed_user = 'jimmytran16'
+    fixed_pass = '68016801'
+    username = request.form['username']
+    password = request.form['password']
+    error = "The username or password you've entered doesn't match any account. Please try again!"
+    if username == fixed_user:
+        if password == fixed_pass:
+            return render_template('dashboard.html', error=False, name = fixed_name)
+    else:
+        return render_template('index.html', error = True, message = error)
+    
+    return render_template('index.html', error = True, message = error)
 
 if __name__ == '__main__':
+    app.secret_key = 'secret123'
     app.run(debug=True)
 
 
