6768150280e13daac779caa1b7cf4a05f0c805bf
Jimmytran16 <jimmytran1620@gmail.com>
2020-01-19 23:57:29 -0500

    done staff autho,done act log

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = 'bhcc'
 app.config['MYSQL_DB'] = 'HHDB'
-#init a mysql object
+# init a mysql object
 mysql = MySQL(app)
 # -----------------------------------
 # Mapping for URLS
@@ -22,11 +22,14 @@ mysql = MySQL(app)
 def index():
     return render_template('index.html')
 
+
 @app.route('/dashboard')
 def dashboard():
     if not autho_login():
         return redirect(url_for("denied"))
-    return render_template('dashboard.html', login= True, name = session['fullname'])
+    activity_log = getActivityLogList()  # get activity log from db
+    print(activity_log)
+    return render_template('dashboard.html', login=True, name=session['fullname'], activity_log=activity_log)
 
 # student behavoir chart
 @app.route('/behavior')
@@ -50,9 +53,9 @@ def forms():
         return render_template('forms_component.html')
     # if not then iterate through the data by rows
     else:
-        #get the BLOB data from rows
-        #convert from binary to string representation
-        #get the filename, then store in local folder
+        # get the BLOB data from rows
+        # convert from binary to string representation
+        # get the filename, then store in local folder
         doc_list = []
         for row in data:
             ext_to_save = 'static/docs/'+row[0]
@@ -60,10 +63,10 @@ def forms():
             file_blob = row[1]
             imgdata = base64.b64decode(file_blob)
             with open(ext_to_save, 'wb') as f:
-                f.write(imgdata) 
+                f.write(imgdata)
             doc_list.append(filename)
         print(doc_list)
-        return render_template('form_component.html',images = doc_list)
+        return render_template('form_component.html', images=doc_list)
     return render_template('form_component.html')
 
 # student records - grades
@@ -82,6 +85,7 @@ def students():
     for rows in data:
         student_info = {}
         print(rows)
+        student_info['student_teacher_id'] = rows[1]
         student_info['student_name'] = rows[0]
         student_info['student_teacher'] = getTeacher(rows[1])
         student_info['student_english'] = rows[2]
@@ -93,39 +97,43 @@ def students():
     # return to table template, and pass in list
     return render_template('basic_table.html', list=student_list)
 # student info page where their grades can be modified by teacher
-@app.route('/student/information',methods=['GET'])
+@app.route('/student/information', methods=['GET'])
 def studentForm():
     if not autho_login():
         return redirect(url_for('denied'))
     else:
         student_name = request.args.get('student_name')
+        teacher_id = request.args.get('autho_id')
         student_id = request.args.get('student_id')
         student_grades = getStudentGrades(student_id)
         student_info = getStudentInfo(student_id)
-        return render_template('studentinfo.html',student=student_name,grades=student_grades,student_info=student_info)
-#ACTIONS------------------------------------------------------------------
+        return render_template('studentinfo.html', student=student_name, grades=student_grades, student_info=student_info, teacher_id=teacher_id)
+# ACTIONS------------------------------------------------------------------
 # upload form submissions
-@app.route('/upload',methods=['POST','GET'])
-def upload(): 
-    #init date object, get date, get filename
-    #convert the file to blob
-    #execute query to upload image into DB
-    #commit the execution
+@app.route('/upload', methods=['POST', 'GET'])
+def upload():
+    # init date object, get date, get filename
+    # convert the file to blob
+    # execute query to upload image into DB
+    # commit the execution
+
+    # update activity log
+    updateActivityLog('2', 'blank')
     date_today = date.today()
-    # updateActivityLog('2');
     file = request.files['filetoupload']
     FILENAME = file.filename
     BLOB = base64.b64encode(file.read())
     cur = mysql.connection.cursor()
     DATE_TO_STORE = date_today.strftime("%b-%d-%Y")
-    cur.execute('INSERT INTO uploads(filename,upload,date,type) values(%s,%s,%s,%s)',(FILENAME,(BLOB,),DATE_TO_STORE,'1'))
+    cur.execute('INSERT INTO uploads(filename,upload,date,type) values(%s,%s,%s,%s)',
+                (FILENAME, (BLOB,), DATE_TO_STORE, '1'))
     mysql.connection.commit()
     cur.close()
     return redirect(url_for('forms'))
 
-#gets the data from form, and updates it into MySQL
-@app.route('/student/update/grades',methods =['POST'])
-def updateGrade(): 
+# gets the data from form, and updates it into MySQL
+@app.route('/student/update/grades', methods=['POST'])
+def updateGrade():
     cur = mysql.connection.cursor()
     student_id = request.form.get('student_id')
     math = request.form.get('math-class')
@@ -133,21 +141,25 @@ def updateGrade():
     ss = request.form.get('ss-class')
     sci = request.form.get('sci-class')
     student_class = request.form.get('class')
+    student_fullname = getStudentName(student_id)
+    updateActivityLog('1', student_fullname)
     query = f'UPDATE student SET english={eng},math={math},science={sci},history={ss} WHERE id_number = {student_id}'
     cur.execute(query)
     mysql.connection.commit()
     cur.close()
     return redirect(url_for('students'))
-@app.route('/docs/form',methods=['GET'])
+# map to expand the forms from form_component
+@app.route('/docs/form', methods=['GET'])
 def renderDoc():
     image_url = request.args.get('imageUrl')
-    return render_template('showdoc.html',image_url = image_url)
+    return render_template('showdoc.html', image_url=image_url)
 # AUTHORIZATIONS-------------------------------------------------------------------
-# block any type of unauthorized bypass and then redirecting to log in page 
+# block any type of unauthorized bypass and then redirecting to log in page
 @app.route('/accessDenied')
 def denied():
-    return render_template('index.html',error=True,message="Please log in!")
-    
+    return render_template('index.html', error=True, message="Please log in!")
+
+
 @app.route('/login', methods=['POST'])
 def login():
     # get cursor object for sql connection
@@ -166,32 +178,38 @@ def login():
     # check if username exist, if yes, verify the pass
     # if fail then return to main, with error message
     for row in data:
-        if row[2] == username:      
+        if row[2] == username:
             if sha256_crypt.verify(password, row[3]):
                 full_name = row[0]+' '+row[1]
-                #save user's name in session
-                session['fullname'] = full_name
+                session['fullname'] = full_name  # save user's name in session
+                session['id'] = row[4]
                 return redirect('dashboard')
             else:
                 return render_template('index.html', error=True, message=err_msg)
     return render_template('index.html', error=True, message=err_msg)
 
+
 @app.route('/logout')
 def logout():
     session['fullname'] = None
+    session['id'] = None
     return redirect('/')
 
-# function to determine if user is logged in or not
+# function to determine if there is a user logged in
+
+
 def autho_login():
-    #session variable 'fullname' is not assigned when user first runs the app
+    # session variable 'fullname' is not assigned when user first runs the app
     if not session.get('fullname'):
         return False
-    elif session['fullname']!=None:
+    elif session['fullname'] != None:
         return True
     else:
         return False
 
-# func to get specific student grades, passing in id as reference
+# func to get a specific student's grades, passing in id as reference
+
+
 def getStudentGrades(st_id):
     cur = mysql.connection.cursor()
     query = f'SELECT * from student where id_number ={st_id};'
@@ -200,33 +218,59 @@ def getStudentGrades(st_id):
     cur.close()
     return data[0]
 # func to get student_info
+
+
 def getStudentInfo(st_id):
     cur = mysql.connection.cursor()
-    query=f'SELECT * from student_info WHERE id_number ={st_id};'
+    query = f'SELECT * from student_info WHERE id_number ={st_id};'
     cur.execute(query)
     data = cur.fetchall()
     return data[0]
-# func to get teacher's name using teacher id foreign key as reference
-def getTeacher(teacher_id):
+# func to get specfic student's full name
+
+
+def getStudentName(st_id):
     cur = mysql.connection.cursor()
-    query = f'SELECT * from teacher where teacherID = {teacher_id};'
+    query = f'SELECT * from student where id_number = {st_id};'
     cur.execute(query)
     data = cur.fetchall()
     return data[0][0]
-# func to create a log of user's activity 
-# def updateActivityLog(type):
-#     cur = mysql.connection.cursor()
-#     current_day = date.today() 
-#     if type == '1':
-#         des = 'Grade has been updated'
-#     elif type =='2'
-#         des = 'A disciplinary/incident form has been submitted. Please take notice!'
-#     query = f"INSERT into log(teacher,activity_log,date,type) values('{}','{des}','{}','{type}');"
-#     cur.execute(query)
-#     mysql.connection.commit()
-#     cur.close()
+# func to get teacher's name using teacher id foreign key as reference
 
-#condition to run the app.py
+
+def getTeacher(teacher_id):
+    cur = mysql.connection.cursor()
+    query = f'SELECT * from staff where staffID = {teacher_id};'
+    cur.execute(query)
+    data = cur.fetchall()
+    return data[0][0]
+# func to create a log of user's activity
+
+
+def updateActivityLog(type, student):
+    cur = mysql.connection.cursor()
+    current_day = date.today()
+    if type == '1':
+        des = f'Grade has been updated for {student}'
+    elif type == '2':
+        des = f'A disciplinary/incident form has been submitted. Please take notice! by-{session["fullname"]}'
+    query = f"INSERT into log(teacher,activity_description,date,type) values('{session['id']}','{des}','{current_day}','{type}');"
+    cur.execute(query)
+    mysql.connection.commit()
+    cur.close()
+# func to return a list of the activity logdesc
+
+
+def getActivityLogList():
+    cur = mysql.connection.cursor()
+    query = 'SELECT * from log'
+    cur.execute(query)
+    data = cur.fetchall()
+    cur.close()
+    return data
+
+
+# condition to run the app.py
 if __name__ == '__main__':
     app.secret_key = '1232131G/21321312sw11'
     app.run(debug=True)
